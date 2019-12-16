961af7f2d25ae6a72ff7c53da92f8fddadbaf74c
Richard Ellicott <mescalin@gmail.com>
2019-12-16 19:16:05 +0000

    .

---
+++ code_patterns/list_files_in_directory.gd
@@ -0,0 +1,21 @@
+"""
+source:
+https://godotengine.org/qa/5175/how-to-get-all-the-files-inside-a-folder
+"""
+
+func list_files_in_directory(path):
+    var files = []
+    var dir = Directory.new()
+    dir.open(path)
+    dir.list_dir_begin()
+
+    while true:
+        var file = dir.get_next()
+        if file == "":
+            break
+        elif not file.begins_with("."):
+            files.append(file)
+
+    dir.list_dir_end()
+
+    return files
