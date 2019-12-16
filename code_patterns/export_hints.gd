4b4023623d33e0e8dafdfe1827e4655fd30f3cbf
Richard Ellicott <mescalin@gmail.com>
2019-12-16 19:51:46 +0000

    ..

---
+++ code_patterns/export_hints.gd
@@ -0,0 +1,12 @@
+"""
+
+export hint examples
+
+
+"""
+
+# export to the local path of a scene file
+export(String, FILE, "*.tscn,*.scn") var scene_file = ""
+
+# export to choose a local folder
+export(String, DIR) var folder_path = ""
