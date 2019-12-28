dbcd8aeaa27eefdbf630ebd60dc1f5d2fb8417d8
Richard Ellicott <mescalin@gmail.com>
2019-12-28 22:49:39 +0000

    Update remove_nulls_from_array.gd

--- arrays/remove_nulls_from_array.gd
+++ arrays/remove_nulls_from_array.gd
@@ -9,6 +9,10 @@ the idea of this is the array is only resized a few times after searching and de
 similar to how lua uses None
 
 
+This is useful as a dynamic programming pattern, the idea is to prevent resizing arrays too often under the hood
+In this pattern setting null is equivalent to deleting a record in a database for example (but the hole is still there)
+
+
 """
 
 func remove_nulls(input_array):
@@ -17,8 +21,8 @@ func remove_nulls(input_array):
     # i.e [1,2,null,3,null,4] => [1,2,3,4]
     # for usage of lua nulls style pattern
     # the array is only resized a few times with this method for better performance
-    
-    var ret = []
+
+    var ret = [] # create a return array
     ret.resize(input_array.size()) # allocate maximum possible size in case no nulls
     var n = 0 # current reference
     for i in range(input_array.size()):
@@ -37,11 +41,11 @@ func remove_match(input_array, _match = -1):
     # [62,-1,33,-1] => [62,33]
     # the array is only resized a few times with this method for better performance
 
-    var ret = input_array.duplicate()
+    var ret = input_array.duplicate() # copy the array in one go
     for i in range(ret.size()):
         if ret[i] == _match:
-            ret[i] = null
-    ret = remove_nulls(ret)
+            ret[i] = null # set null instead of deleting each time of rebuilding a new array step by step
+    ret = remove_nulls(ret) # strip the nulls with the null stripping function
     return ret
 
 
