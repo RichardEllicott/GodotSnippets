894863008e28614d3e7e37e784c8650d37c8885e
Richard Ellicott <mescalin@gmail.com>
2019-12-28 22:41:14 +0000

    Update remove_nulls_from_array.gd

--- arrays/remove_nulls_from_array.gd
+++ arrays/remove_nulls_from_array.gd
@@ -12,11 +12,12 @@ similar to how lua uses None
 """
 
 func remove_nulls(input_array):
+
     # returns a new list with all the nulls removed
     # i.e [1,2,null,3,null,4] => [1,2,3,4]
     # for usage of lua nulls style pattern
-    # the return array is only resized a few times for speed
-
+    # the array is only resized a few times with this method for better performance
+    
     var ret = []
     ret.resize(input_array.size()) # allocate maximum possible size in case no nulls
     var n = 0 # current reference
@@ -27,3 +28,20 @@ func remove_nulls(input_array):
             n += 1
     ret.resize(n) # chop array to final size
     return ret
+
+
+func remove_match(input_array, _match = -1):
+
+    # example of using remove_nulls pattern
+    # i.e removing the -1's results in:
+    # [62,-1,33,-1] => [62,33]
+    # the array is only resized a few times with this method for better performance
+
+    var ret = input_array.duplicate()
+    for i in range(ret.size()):
+        if ret[i] == _match:
+            ret[i] = null
+    ret = remove_nulls(ret)
+    return ret
+
+
