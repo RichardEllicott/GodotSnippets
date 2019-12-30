a2ce93481e2769ee7931cef2f3859d25fd10aaff
Richard Ellicott <mescalin@gmail.com>
2019-12-30 05:00:05 +0000

    Create random_timer.gd

---
+++ code_patterns/random_timer.gd
@@ -0,0 +1,19 @@
+"""
+
+quick paste timer
+
+
+"""
+
+
+export var delay = 1.0
+export var random_delay  = 1.0
+var timer = 0.0
+var current_delay = 0.0
+
+func _process(delta):
+    timer += delta
+    if timer > current_delay:
+        timer -= current_delay
+        current_delay = delay + randf() * random_delay
+        _ready()
