d2119e4e5c1bbf325f808d3f3950bab73beb6bd7
Richard Ellicott <mescalin@gmail.com>
2019-12-30 05:07:23 +0000

    Update random_gaussian_2D.gd

--- maths/random_gaussian_2D.gd
+++ maths/random_gaussian_2D.gd
@@ -1,12 +1,18 @@
+"""
 
-func ran_gaussian_2D():
+useful for randomly distributing things naturally in 2D, can be used for a shotgun blast for example
+
+"""
+
+func ran_gaussian_2D(r1 = randf(), r2 = randf()):
     """
     ported from the caltech lua one (i think, the link is dead!)
     http://www.design.caltech.edu/erik/Misc/Gaussian.html
 
+    r1 and r2 are floats from 0 to 1
+
+    ran_gaussian_2D(randf(),randf())
     """
-    var r1 = randf()
-    var r2 = randf()
 
     var al1 = sqrt(-2 * log(r1)) # part one
     var al2 = 2 * PI * r2 # part two
