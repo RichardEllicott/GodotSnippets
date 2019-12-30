9b8df53d6e6029f5aa7a54b23cbd33dee5381d1a
Richard Ellicott <mescalin@gmail.com>
2019-12-30 05:10:44 +0000

    Update random_gaussian_2D.gd

--- maths/random_gaussian_2D.gd
+++ maths/random_gaussian_2D.gd
@@ -4,7 +4,6 @@ useful for randomly distributing things naturally in 2D, can be used for a shotg
 
 """
 
-
 func ran_gaussian_2D(r1 = randf(), r2 = randf()):
     """
     ported from the caltech lua one (i think, the link is dead!)
