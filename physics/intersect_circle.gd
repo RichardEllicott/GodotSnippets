fe0ed02ca622d9c9fd857cac167628b7fbdc4281
Richard Ellicott <mescalin@gmail.com>
2019-12-30 04:17:48 +0000

    Update intersect_circle.gd

--- physics/intersect_circle.gd
+++ physics/intersect_circle.gd
@@ -1,10 +1,9 @@
 """
 
-an easy to use physics query, that just does a circle
+an easy to use physics query, creates the circle automatically
 
 
 """
-
 
 var scan_pars # cache the query pars
 var scan_shape # cache the query shape to change radius
@@ -27,3 +26,20 @@ func intersect_circle(_position,_radius):
     scan_pars.transform.origin = _position
 
     return physics_state.intersect_shape(scan_pars)
+
+
+# for debugging, this will draw in the same position as the query
+func _draw_circle(_position,_radius):
+
+    # draw a circle in query position for debugging, note resets the drawing transform
+
+    #hack draws in the global transform
+    #https://github.com/godotengine/godot/issues/5428
+    var inv = get_global_transform().inverse()
+    draw_set_transform(inv.get_origin(), inv.get_rotation(), inv.get_scale())
+    draw_circle(_position,_radius,Color(1,0,0,0.25))
+
+
+
+
+
