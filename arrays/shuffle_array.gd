99da41f9a8a3cbc9aeddec7e81f06805e7487e09
Richard Ellicott <mescalin@gmail.com>
2019-12-31 21:09:15 +0000

    Update shuffle_array.gd

--- arrays/shuffle_array.gd
+++ arrays/shuffle_array.gd
@@ -2,25 +2,29 @@
 
 shuffle an array similar to python's shuffle
 
-shuffle_array(array) # shuffles array different each time using the default RandomNumberGenerator
-shuffle_array(array,seed_number) # shuffles with a seed number, 2638, allowing the shuffle to occur the same each time
+
+there is already a built in shuffle in Godot, but I've kept this for seeded shuffles
+
+this function does not affect the seed of the RandomNumberGenerator as it creates it's own one
 
 """
 
 func shuffle_array(array, _seed = null):
-    
+
     # returns a shuffled copy of an array
     # if _seed is not null
     # will use a seed for procedural results
-    
+
     array = array.duplicate()
     var rand_gen
     if _seed != null:
         rand_gen = RandomNumberGenerator.new()
         rand_gen.set_seed(_seed)
-    var shuffledList = []
-    var indexList = range(array.size())
-    var ran_i
+
+    var shuffled_array = [] # return this
+    shuffled_array.resize(array.size()) # allocate correct length
+
+    var ran_i # random integer
 
     for i in range(array.size()):
         if _seed == null: # if no seed was provided use default random
@@ -28,9 +32,10 @@ func shuffle_array(array, _seed = null):
         else:
             ran_i = rand_gen.randi()
 
-        var array_pos = ran_i % indexList.size()
-        shuffledList.append(array[array_pos])
-        indexList.remove(array_pos)
-        array.remove(array_pos)
+        var array_pos = ran_i % array.size()
 
-    return shuffledList
+        shuffled_array[i] = array[array_pos]
+
+        array.remove(array_pos) # a bit slower maybe
+
+    return shuffled_array
