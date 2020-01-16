c25a2b7911fa36d8ecab8dbbd1db4cbd3cdf60ed
Richard Ellicott <mescalin@gmail.com>
2020-01-16 01:44:47 +0000

    Update get_children_recursive.gd

--- code_patterns/get_children_recursive.gd
+++ code_patterns/get_children_recursive.gd
@@ -10,8 +10,8 @@ this pattern works by recursion (passing itself back to itself)
 
 func get_children_recursive(root, depth = 0):
     var array = []
-    if depth < 8:
-        for child in root.get_children():
-            array.append(child)
+    for child in root.get_children():
+        array.append(child)
+        if depth < 8:
             array += get_children_recursive(child, depth + 1) # append results of recursing on children of the child
     return array
