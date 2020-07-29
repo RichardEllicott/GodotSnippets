86df4ab9e6fa35541f145454553bb090f189a2f3
Richard Ellicott <mescalin@gmail.com>
2020-07-29 23:25:28 +0100

    .

--- code_patterns/save_load_text_files.gd
+++ code_patterns/save_load_text_files.gd
@@ -9,6 +9,9 @@ https://godotengine.org/qa/52307/can-you-read-a-textfile-resource-with-gdscript
 
 
 func load_text_file(path):
+    """
+    load text file return string
+    """
     var f = File.new()
     var err = f.open(path, File.READ)
     if err != OK:
@@ -20,6 +23,9 @@ func load_text_file(path):
 
 
 func save_text_file(text, path):
+    """
+    save string as text file
+    """
     var f = File.new()
     var err = f.open(path, File.WRITE)
     if err != OK:
@@ -30,4 +36,23 @@ func save_text_file(text, path):
 
 
 
-export(String, FILE, "*.txt,*.csv") var system_names_file = "" # create an export link to a txt or csv file in ide
+export(String, FILE, "*.txt,*.csv") var system_names_file = "" # create an export link to a txt or csv file for use in the ide
+
+
+
+func load_csv_file(path):
+    """
+    load a csv file to nested lists like [["Bob", 21], ["Greg", 35]] using built in csv reader
+    """
+    var ret = []
+    var f = File.new()
+    var err = f.open(path, File.READ)
+    if err != OK:
+        printerr("Could not open file, error code ", err)
+        return ret
+    while !f.eof_reached():
+        var csv = f.get_csv_line ()
+        ret.append(csv)
+    return ret
+
+
