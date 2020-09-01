5ec1775c97d44ce0c82031cf02b16d2fbb7e9c88
Richard Ellicott <mescalin@gmail.com>
2020-09-02 00:47:22 +0100

    Update trigger_update_in_tool_mode.gd

--- code_patterns/trigger_update_in_tool_mode.gd
+++ code_patterns/trigger_update_in_tool_mode.gd
@@ -13,6 +13,9 @@ NOTE, we only use a "set" as there is no point to get this variable
 
 """
 
+
+# creates in the ide, a boolean tick box that when you try to set, instead triggers an update but remains false
+
 tool
 extends Node
 
@@ -30,6 +33,26 @@ func tool_update():
 	_ready() # ensure vars are available
     print(self, "run tool_update functions here...")
 
+
+# Creates in the editor a mode menu, that triggers updates on setting the mode
+
+enum Mode {
+    mode1,
+    mode2,
+    mode3,
+   }
+
+export(Mode) var mode = false setget set_mode
+func set_mode(input):
+    mode = input
+    print("Mode set to \"%s\", trigger update!" % [mode])
+    match mode:
+    	Mode.mode1:
+    		pass
+    	Mode.mode2:
+    		pass
+    	Mode.mode3:
+    		pass
 
 
 
