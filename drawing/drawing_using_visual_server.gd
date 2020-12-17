9c64a817c64b07771551d71a756ca59bc19d6720
Richard Ellicott <mescalin@gmail.com>
2020-12-17 19:08:54 +0000

    .

--- drawing/drawing_using_visual_server.gd
+++ drawing/drawing_using_visual_server.gd
@@ -13,13 +13,12 @@ the "layers" property creates independent layers, last created are at top
 
 """
 
-
 var vs_ci_rids # saves a dictionary of canvas layers
 
 func get_vs_ci_rid(layer = 0):
-	"""
-	returns a layer ci_rid, creates a new layer if non exists
-	"""
+    """
+    returns a layer ci_rid, creates a new layer if non exists
+    """
     if not vs_ci_rids: # if no canvas dict
         vs_ci_rids = {}
     if not layer in vs_ci_rids: # if we need a new layer create one
@@ -36,9 +35,6 @@ func vs_clear_resources():
             var cid = vs_ci_rids[key]
             VisualServer.canvas_item_clear(cid)
 
-
-
-
 # clear a canvas layer
 func vs_clear_canvas(layer=0):
     VisualServer.canvas_item_clear(get_vs_ci_rid(layer))
@@ -50,8 +46,6 @@ func vs_draw_rect(rect,color=Color.red,layer=0):
 # draw a line
 func vs_draw_line(from,to,color=Color.red,width=1.0,layer=0):
     VisualServer.canvas_item_add_line(get_vs_ci_rid(layer),from,to,color,width,true)
-
-
 
 func vs_draw_grid(rect,color=Color.red,width=1.0,div=8,layer=0,draw_horizontal=true,draw_vertical=true):
     """
@@ -82,34 +76,25 @@ func vs_draw_grid(rect,color=Color.red,width=1.0,div=8,layer=0,draw_horizontal=t
             var end = Vector2(rect.size.x,ypos) + rect.position
             vs_draw_line(start,end,color,width,layer)
 
-
-
 func vs_draw_texture_rect(texture,rect,modulate=Color.white,layer=0):
-	"""
-	put an export in the script:
-		export(Texture) var texture
-	then load it with this function
-
-	https://docs.godotengine.org/en/stable/classes/class_visualserver.html#class-visualserver-method-canvas-item-add-texture-rect-region
-	"""
+    """
+    put an export in the script:
+        export(Texture) var texture
+    then load it with this function
+    https://docs.godotengine.org/en/stable/classes/class_visualserver.html#class-visualserver-method-canvas-item-add-texture-rect-region
+    """
 
     VisualServer.canvas_item_add_texture_rect(get_vs_ci_rid(layer), rect, texture,false,modulate)
-
-
-
 
 func _notification(what):
     if what == NOTIFICATION_PREDELETE:
         # called when the node is destroyed
         vs_clear_resources() # clear the VisualServer canvases
 
-
-
-
-func _ready():
-	"""
-	usage example
-	"""
-	vs_draw_rect(Rect2(0,0,16,16),Color.blue)
+func vs_usage_example():
+    """
+    usage example
+    """
+    vs_draw_rect(Rect2(0,0,16,16),Color.blue)
     vs_draw_rect(Rect2(16,16,16,16),Color.red)
 
