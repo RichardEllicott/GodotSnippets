371783470577bcf059441f8eb81609a04a6b184c
Richard Ellicott <mescalin@gmail.com>
2021-01-06 18:57:46 +0000

    Update list_files_in_dir.gd

--- code_patterns/list_files_in_dir.gd
+++ code_patterns/list_files_in_dir.gd
@@ -18,9 +18,28 @@ func list_files_in_directory(path):
         var file = dir.get_next()
         if file == "":
             break
-        elif not file.begins_with("."):
-            files.append(file)
+        files.append(file)
 
     dir.list_dir_end()
 
     return files
+
+
+
+        """
+        matching start and fin of filenames with regex (saved for refactor)
+
+        can't seem to get match groups working (if they exist in godot)
+
+        var re = "TROO"
+        var regex = RegEx.new()
+        # ^ assert starts
+        # .* is wildchar
+        # \\. escape char to match .
+        # $ assert start
+        regex.compile("^" + re + ".*" + "\\.png$")
+        var result = regex.search(file)
+        if result:
+#            print("match: ", file)
+            print(result.get_strings())
+        """
