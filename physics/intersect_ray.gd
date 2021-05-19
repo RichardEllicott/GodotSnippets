5652472f6be7c47af7f5993df434be52d875d378
Richard Ellicott <mescalin@gmail.com>
2021-05-19 21:54:49 +0100

    .

--- physics/intersect_ray.gd
+++ physics/intersect_ray.gd
@@ -2,4 +2,6 @@
 """
 
 
-var ray = get_world().get_direct_space_state().intersect_ray(pos1, pos2,exclude=[self])
+var pos1 = Vector3()
+var pos2 = Vector3(16,0,0)
+var ray = get_world().get_direct_space_state().intersect_ray(pos1, pos2,[self])
