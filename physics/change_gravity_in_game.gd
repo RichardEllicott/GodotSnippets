38d557d1106923d327a8c6ebd24f3a61742cc295
Richard Ellicott <mescalin@gmail.com>
2021-06-05 15:31:36 +0100

    Update change_gravity_in_game.gd

--- physics/change_gravity_in_game.gd
+++ physics/change_gravity_in_game.gd
@@ -4,6 +4,9 @@
 
 # change the gravity vector midgame
 
+# i use this for pinball for example, to simulate a 6.5 degree slope: Vector3(0, -0.994, 0.113)
+
+
 PhysicsServer.area_set_param(
     get_world().space,
     PhysicsServer.AREA_PARAM_GRAVITY_VECTOR,
