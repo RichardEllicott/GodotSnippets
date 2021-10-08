343c4a26b0a1324ce66f60c69b0d1c69672cb280
Richard Ellicott <mescalin@gmail.com>
2021-10-08 22:26:28 +0100

    Update get_children_recursive.gd

--- code_patterns/get_children_recursive.gd
+++ code_patterns/get_children_recursive.gd
@@ -10,12 +10,12 @@ func get_children_recursive(root, _depth = 0, _node_list = null):
     recursively find all children of node "root" and return them as a list
     do not use the variables _depth and _node_list which are for passing back data in recursion
     usage examples:
-        recursive_get_nodes(self) # get all children of this node
-        recursive_get_nodes(get_tree().get_root()) # get all children of top node
+        get_children_recursive(self) # get all children of this node
+        get_children_recursive(get_tree().get_root()) # get all children of top node
     """
     if not _node_list:
         _node_list = []
     for child in root.get_children():
         _node_list.append(child)
-        recursive_get_nodes(child, _depth + 1, _node_list)
+        get_children_recursive(child, _depth + 1, _node_list)
     return _node_list
