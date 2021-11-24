519573c720e41d248c81d31d2d464350a724515a
Richard Ellicott <mescalin@gmail.com>
2021-11-24 00:50:53 +0000

    Create add_spatial_if_not_present.gd

---
+++ code_patterns/add_spatial_if_not_present.gd
@@ -0,0 +1,20 @@
+"""
+
+add a child spatial node if not already present
+
+
+"""
+
+
+
+func add_spatial(spatial_name = "NewSpatial"):
+    
+    var existing_node = get_node(spatial_name)
+    
+    if not is_instance_valid(existing_node):
+        var new_node = Spatial.new()
+        new_node.name = spatial_name
+        add_child(new_node)
+        
+        if Engine.editor_hint: # if in editor make visible
+            new_node.set_owner(get_tree().get_edited_scene_root())
