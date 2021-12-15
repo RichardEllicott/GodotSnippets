edb2fdc9af02dd13c88f1f937c5e7cce53968607
Richard Ellicott <mescalin@gmail.com>
2021-12-15 00:38:03 +0000

    Update and rename seconds_to_time_display.gd to timestamp_to_timestring.gd

---
+++ code_patterns/timestamp_to_timestring.gd
@@ -0,0 +1,15 @@
+"""
+
+timestamp_to_timestring
+
+0 => 1970/01/01 00:00
+
+
+"""
+
+
+
+func timestamp_to_timestring(number) -> String:
+    var time : Dictionary = OS.get_datetime_from_unix_time(number);
+    var display_string : String = "%d/%02d/%02d %02d:%02d" % [time.year, time.month, time.day, time.hour, time.minute];
+    return display_string
