b7dafa106993b00cfb3a73c026510634f76209b5
Richard Ellicott <mescalin@gmail.com>
2021-12-22 16:05:44 +0000

    Update and rename relative_rotation_about_spheres.gd to relative_rotation.gd

--- code_patterns/relative_rotation.gd
+++ code_patterns/relative_rotation.gd
@@ -6,24 +6,21 @@ scraps while solving how to make a geoscape like xcom (units flying around on sp
 """
 
 
-rotate_object_local(Vector3(1, 0, 0),  delta * speed * velocity.x)
+func rotate_local(rot: Vector3): # to rotate locally, for example you have a space ship/air plane control with complete rotational freedom (yaw, pitch, roll)
 
-
-
-func rotate_local(rot: Vector3):
-
-    # using the basis vectors
+    # you could rotate using a basis as up
     rotate(transform.basis.x, rot.x) # forwards N
     rotate(transform.basis.y, rot.y) # strafe E
     rotate(transform.basis.z, rot.z) # rotate clockwise direction
 
-    # alternative
+    # alternative, use dedicated function, this method is perhaps more flexible as you could create a custom axis
     rotate_object_local(Vector3(1, 0, 0),  rot.x)
     rotate_object_local(Vector3(0, 1, 0),  rot.y)
     rotate_object_local(Vector3(0, 0, 1),  rot.z)
 
 
-# Stereographic_projection formulae:
+# Stereographic_projection formulae (UNTESTED, UNSURE) should translate a 2D equirectangular map position to a position on surface of sphere:
+
 func sphere_to_plane_pos(input: Vector3) -> Vector2:
     # https://en.wikipedia.org/wiki/Stereographic_projection
     
