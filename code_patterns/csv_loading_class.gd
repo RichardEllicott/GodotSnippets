59130fd6c35c82e4ccecaed64232a6e2f081d3f3
Richard Ellicott <mescalin@gmail.com>
2021-12-23 23:51:00 +0000

    Update csv_loading_class.gd

--- code_patterns/csv_loading_class.gd
+++ code_patterns/csv_loading_class.gd
@@ -7,6 +7,9 @@ a class to load CSV tables, my existing functions are not suitable for all purpo
 
 """
 
+
+
+
 class CSV_Table:
     """
     
@@ -16,8 +19,7 @@ class CSV_Table:
     
     """
     
-    
-    var _filename
+    var _filename # csv filename
     
     var _key_to_col = {} # convert a column name to a column int reference
     
@@ -25,9 +27,15 @@ class CSV_Table:
     
     var records = [] # list of rows of strings
     
+    var max_rows = null
     
-    func load_filename(_filename, max_rows = 10):
+    func load_filename(_filename, max_rows = 92233720368547758):
         
+        
+        print("CSV_Table LOAD FILE ", _filename)
+        
+
+            
         
         records = []
         
@@ -76,26 +84,70 @@ class CSV_Table:
                     
                     record_ref += 1
         
-    
-    
-    func _init(_filename):
-        self._filename = _filename
-        
-        load_filename(_filename)
-        
-        
-        print("CSV_Table record: %s" % [records.size()])
-    
-
+        print("CSV_Table FILE LOADED ", _filename)
 
     func get_list_of_dicts():
-        var ret = []
-        
-        for record in records:
-            
-            pass
-        
-        
-        pass
+        """
+        return the csv table files data as a list of dicts
+        """
+        var list_of_dicts = [] # retiurn values
+        for record in records: # for each record
+            var row = {} # build a row
+#            print("row: ", row)
+            print("rowsize: ", row.size())
+            list_of_dicts.append(row) # append to ret
+            for i in header.size(): # for each col
+                
+                
+                print("header[i]: ", header[i])
+                print("record[i]: ", record[i])
+                
+                
+                
+                row[header[i]] = record[i] # assign the value to the dict
+        return list_of_dicts
 
+    
+    func get_keyed_data(primary_key : int = 0):
+        """
+        return the data as a dict of dicts, specifying which column to take as the primary key
+        
+        
+        NOT WORKING!!!
+        """
+        
+        var ret = {} # return dict of dicts
+        
+        
+        for rec_ref in records.size(): # for each record
+            
+            var record = records[rec_ref] # record data as a list
+            
+            var dict_ref = null # the primary key will be found and put here (usually column 0)
+            
+            var record_dict = {} # dict of the record to return
+            
+            for col_ref in record.size(): # for each column
+                var val = record[col_ref] # the cell value
+                
+                var key = header[col_ref] # the column name
+                
+                if col_ref == primary_key: # if we are the primary key
+                    dict_ref = val # record the primary key val
+                    
+                record_dict[key] = val
+                
+            ret[dict_ref] = record_dict # save the record dict using the found key
+                    
+        return ret
+        
+        
+
+            
+
+    func _init(_filename: String):
+        self._filename = _filename
+        load_filename(_filename,5)
+    
+        
 
