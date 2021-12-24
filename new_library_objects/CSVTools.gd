71f044f77c65c7ecfb577f38188550caee411383
Richard Ellicott <mescalin@gmail.com>
2021-12-24 21:02:29 +0000

    Update CSVTools.gd

--- new_library_objects/CSVTools.gd
+++ new_library_objects/CSVTools.gd
@@ -5,6 +5,7 @@
 ## csv_table.get_list_of_dicts() # get a list of rows as dicts
 ## csv_table.get_keyed_data() # get a dict of dicts using the first column as the primary key
 ##
+## as per Godot's built in csv reading, this class uses unicode (utf-8)
 
 class_name CSVTools
 
@@ -17,25 +18,22 @@ class CSVTable:
 
     var header : Array = [] # list of strings from header
 
-    var records : Array = [] # list of rows of strings
+    var body : Array = [] # list of list of strings (the main csv data)
 
     var _key_to_col : Dictionary = {} # convert a column name to a column int reference
 
+    ## loads a filename, called internally, do not use
+    ## stores the data of the csv file into this object for further usage
     func _load_filename(_filename, max_rows):
-        """
-        loads a filename to this object
-            
-        filename should be a csv table file with a top row as the header
-        """
         
-        records = []
+        # reset these variables
+        _filename = _filename 
+        header = []
+        body = []
+        _key_to_col = {}
         
         var record_ref = 0
-        
         var start_row = 0
-        self._filename = _filename
-        
-        _key_to_col = {}
         
         var f = File.new()
         var err = f.open(_filename, File.READ)
@@ -62,28 +60,37 @@ class CSVTable:
                     header = row
 
                 else: # records
-                    records.append(row)               
+                    body.append(row)               
                     record_ref += 1
+                    
+                    
+    ## get a record row as a dictionary
+    func get_row_as_dict(row = 0):
+        var record = body[row] # the record as a array
+        var ret = {} # return dict
+        for i in header.size():
+            ret[header[i]] = record[i]
+        return ret
+    
     
     ## return a list of record rows as dictionaries (similar to python DictReader)
     func get_list_of_dicts():
-        
         var list_of_dicts = [] # retiurn values
-        for record in records: # for each record
-            var row = {} # build a row
-            list_of_dicts.append(row) # append to ret
-            for i in header.size(): # for each col
-                row[header[i]] = record[i] # assign the value to the dict
+        
+        for i in body.size():
+            list_of_dicts.append(get_row_as_dict(i))
         return list_of_dicts
+    
+
 
     ## return a dict of dicts, such that the first column serves as a key reference
     func get_keyed_data(primary_key : int = 0):
         
         var ret = {} # return dict of dicts
         
-        for rec_ref in records.size(): # for each record
+        for rec_ref in body.size(): # for each record
             
-            var record = records[rec_ref] # record data as a list
+            var record = body[rec_ref] # record data as a list
             
             var dict_ref = null # the primary key will be found and put here (usually column 0)
             
@@ -118,6 +125,7 @@ class CSVTable:
             input += " "
 
         return input
+        
     
     ## return the data as an ASCII style table for debugging
     func get_pretty_string(col_width = 12):
@@ -150,7 +158,7 @@ class CSVTable:
         ret += "---"
         ret += "\n"
     
-        for row in records:
+        for row in body:
             
             for val in row:
                 
@@ -168,12 +176,8 @@ class CSVTable:
         ret += "\n"
         
         return ret
-            
-    func _init(_filename: String, max_rows):
-        self._filename = _filename
-        _load_filename(_filename,max_rows)
-    
-    ## save the data from header and records into a new csv file
+        
+   ## save the data from header and records into a new csv file
     func save_to_file(_filename):
 
         var file = File.new()
@@ -184,7 +188,14 @@ class CSVTable:
             
             file.store_csv_line(header)
             
-            for record in records:
+            for record in body:
                 file.store_csv_line(record)
         file.close()
+    
+    ## when loading class, we need to specify the csv filename which will trigger loading
+    func _init(_filename: String, max_rows = 1000000):
+        self._filename = _filename
+        _load_filename(_filename,max_rows)
+    
+ 
     
