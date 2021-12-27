8d8f6bd7881a91a8966fe62826e365ef9a0bcde0
Richard Ellicott <mescalin@gmail.com>
2021-12-27 23:49:49 +0000

    Update selecting_target_nodes_in_editor.gd

--- code_patterns/selecting_target_nodes_in_editor.gd
+++ code_patterns/selecting_target_nodes_in_editor.gd
@@ -20,3 +20,24 @@ extends Node
 
 export(NodePath) var _target_node
 onready var target_node = get_node(_target_node)
+
+
+
+
+# trigger update when setting variable, works in tool mode
+export var target_node2 = false setget set_target_node2
+
+func set_target_node2(new_value):
+    show_coordinates = new_value
+   
+	
+	
+	
+# get only property that works in tool mode, make sure though to call get_target3() locally
+export(NodePath) var _target3
+var target3 setget , get_target3
+
+func get_target3():
+    if not target3:
+        target3 = get_node(_target3)
+    return target3
