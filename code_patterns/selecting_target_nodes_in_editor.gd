d5a889047924921dfb06b65809cfffcce128d92f
Richard Ellicott <mescalin@gmail.com>
2022-01-03 22:31:09 +0000

    Update selecting_target_nodes_in_editor.gd

--- code_patterns/selecting_target_nodes_in_editor.gd
+++ code_patterns/selecting_target_nodes_in_editor.gd
@@ -22,6 +22,20 @@ export(NodePath) var _target_node
 onready var target_node = get_node(_target_node)
 
 
+# ALTERNATIVE FOR TOOl SCRIPTS:
+
+export(NodePath) var _target_node
+var target_node setget , get_target_node # only get
+func get_target_node():
+    if not target_node: # if no ref
+        if not _target_node.is_empty(): # check NodePath not empty
+            target_node = get_node(_target_node)
+    return target_node
+
+
+
+
+
 
 
 # trigger update when setting variable, works in tool mode
@@ -32,12 +46,8 @@ func set_target_node2(new_value):
    
 	
 	
-	
-# get only property that works in tool mode, make sure though to call get_target3() locally
-export(NodePath) var _target3
-var target3 setget , get_target3
 
-func get_target3():
-    if not target3:
-        target3 = get_node(_target3)
-    return target3
+
+
+
+
