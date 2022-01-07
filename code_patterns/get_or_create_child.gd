b5fc7900330eb5fa735ceef27c7205ca233642f8
Richard Ellicott <mescalin@gmail.com>
2022-01-07 17:59:35 +0000

    Update get_or_create_child.gd

--- code_patterns/get_or_create_child.gd
+++ code_patterns/get_or_create_child.gd
@@ -10,6 +10,10 @@ quickly create a child node of type if missing (works in tool mode)
 ## get a child, or create if not available (cannot be static)
 ## example:
 ##     var node = get_or_create_child(self,"CITIES",Spatial)
+##     
+##     var node1 = get_or_create_child(self,"child")
+##     var node2 = get_or_create_child(node1,"sub_child") # create child of child like making directories
+##
 func get_or_create_child(parent: Node,node_name: String, node_type = Node) -> Node:
         
     var node_ref = parent.get_node_or_null(node_name) # get the node if present
