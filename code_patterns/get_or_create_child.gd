4defad300c35c550d227cbbb8e2be264a1383242
Richard Ellicott <mescalin@gmail.com>
2022-01-07 17:28:08 +0000

    Update and rename get_or_create_node.gd to get_or_create_child.gd

---
+++ code_patterns/get_or_create_child.gd
@@ -0,0 +1,23 @@
+"""
+
+quickly create a child node of type if missing (works in tool mode)
+
+
+"""
+
+
+## get a child, or create if not available (cannot be static)
+## example:
+##     var node = get_or_create_child(self,"CITIES",Spatial)
+func get_or_create_child(parent: Node,node_name: String, node_type = Spatial) -> Node:
+        
+    var node_ref = parent.get_node_or_null(node_name) # get the node if present
+    if not is_instance_valid(node_ref): # if no node found make one
+        node_ref = node_type.new()
+        node_ref.name = node_name
+        parent.add_child(node_ref)
+        node_ref.set_owner(get_tree().edited_scene_root) # show in tool mode
+        
+    assert(node_ref is node_type) # best to check the type matches
+    
+    return node_ref
