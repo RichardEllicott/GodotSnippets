4e748cec8f89b4d7b693627d6125de89f90ee03f
Richard Ellicott <mescalin@gmail.com>
2022-02-08 22:43:12 +0000

    Update 2D_Array_PoolArray_Wrapper.gd

--- arrays/2D_Array_PoolArray_Wrapper.gd
+++ arrays/2D_Array_PoolArray_Wrapper.gd
@@ -37,29 +37,33 @@ class TwoDimensionalArray:
     
     var pool : PoolRealArray
     
-    var _width : int
+    var _width : int # private vars
     var _height : int
     
+    ## return the pool array reference number from (x,y) data
     func _get_cell_ref(x : int, y : int) -> int:
         
-        assert(x < _width)
+        assert(x < _width) # force a crash if out of range query is made, this is better than wacky return data
         assert(y < _height)
         
         return x + (y * _width)
     
+    ## return a cell value
     func get_cell(x : int, y : int) -> float:
         return pool[_get_cell_ref(x,y)]
         
+    ## set a cell value
     func set_cell(x : int, y : int, val : float):
         pool[_get_cell_ref(x,y)] = val
         
+    ## clear all cells by overwiting them (default with 0.0)
     func clear(default_value : float = 0.0):
         for y in _height:
             for x in _width:
                 set_cell(x,y,default_value)
-        
     
-    func _init(width = 16, height = 16):
+    ## we require the width and height to build the object
+    func _init(width, height):
         pool = PoolRealArray() # create a new pool
         pool.resize(width*height) # set it's size in advance (warning garbage data now present)
         
