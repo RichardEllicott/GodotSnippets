e3e78cfec9b8c58b5a0bd88882bd57980709fd3f
Richard Ellicott <mescalin@gmail.com>
2022-07-25 21:26:44 +0100

    Update get_or_create_child.gd

--- code_patterns/get_or_create_child.gd
+++ code_patterns/get_or_create_child.gd
@@ -3,22 +3,24 @@
 quickly create a child node of type if missing (works in tool mode)
 
 
+this can make setting up a nodetree with children very easy:
+
+var panel : Panel = get_or_create_node('Panel', Panel)
+panel.rect_min_size = Vector2(128,64)
+var label : Label = get_or_create_node('Panel/Label', Label)
+label.text = "hello godot!"
+
+
+it's like calling "get_node_or_null" where it would never return null as it will create the object if required
+this allows a very fast dynamic setup... i find it's better than notes to make a script set itself up to a minimum level adding required child nodes if they don't exist
+
 """
 
 
 
-## get a child, or create if not available (cannot be static)
-## example:
-##     var node = get_or_create_child(self,"CITIES",Spatial)
-##     
-##     var node1 = get_or_create_child(self,"child")
-##     var node2 = get_or_create_child(node1,"sub_child") # create child of child like making directories
-##
-
-
-
-## best version so far
-## i would like to just type in a nodepath but you need to feed the parents in here
+## get or create a child node, example:
+## var mylabel = get_or_create_child(self,"MyLabel",Label) ## child of this node
+## var mylabel = get_or_create_child(mylabel,"ChildLabel",Label) ## sub child
 func get_or_create_child(parent: Node,node_name: String, node_type = Node) -> Node:
         
     var node = parent.get_node_or_null(node_name) # get the node if present
@@ -31,12 +33,26 @@ func get_or_create_child(parent: Node,node_name: String, node_type = Node) -> No
     assert(node is node_type) # best to check the type matches
     
     return node
+
+
+## using the previous function but making usage a bit more simple
+## we can add a full nodepath, make sure to add in correct order:
+
+## var panel : Panel = get_or_create_node('Panel', Panel)
+## var label : Label = get_or_create_node('Panel/Label', Label)
+func get_or_create_node(nodepath : String, node_type = Node) -> Node:
+    
+    var split : PoolStringArray = nodepath.split('/')
+    var node : Node
+    var parent : Node = self
+    var node_name : String = nodepath
+    
+    if split.size() > 1: # if we have any '/' chars we need to seperate the paths and name
+        node_name = split[-1] # the name is the last entry
+        split.resize(split.size()-1) # drop the last entry
+        var parent_path = split.join('/') # then combine for parent path   
+        parent = get_node_or_null(parent_path)
         
-
-
-## example usage of get_or_create_child: 
-var _control_nodes
-func get_control_nodes():
-    if not is_instance_valid(_control_nodes):
-      _control_nodes = get_or_create_child(self,"CONTROL",Node)
-    return _control_nodes
+    node = get_or_create_child(parent,node_name,node_type)
+    
+    return node
