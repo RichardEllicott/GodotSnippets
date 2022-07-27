db7bd8527f5b0ba4483f96078e7352c675200e65
Richard Ellicott <mescalin@gmail.com>
2022-07-27 17:45:28 +0100

    Update eval_expressions.gd

--- code_patterns/eval_expressions.gd
+++ code_patterns/eval_expressions.gd
@@ -1,12 +1,20 @@
 
 
-func eval_expression(command : String):
+
+
+## example of evaluating using variables from a dictionary:
+static func evaluate_with_dict(command, variables : Dictionary, ref = null):
+    return evaluate(command,ref,variables.keys(),variables.values())
+
+## evaluate all options, 
+static func evaluate(command, ref = null, variable_names = [], variable_values = []):
     ## https://docs.godotengine.org/en/stable/classes/class_expression.html
+    ## https://docs.godotengine.org/en/stable/tutorials/scripting/evaluating_expressions.html
     var expression = Expression.new()
-    var error = expression.parse(command, [])
+    var error = expression.parse(command, variable_names)
     if error != OK:
-        print(expression.get_error_text())
+        push_error(expression.get_error_text())
         return
-    var result = expression.execute([], null, true)
+    var result = expression.execute(variable_values, ref) # show error?
     if not expression.has_execute_failed():
-        return str(result)
+        return result
