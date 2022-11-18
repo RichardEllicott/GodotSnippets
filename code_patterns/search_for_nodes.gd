f07a308a5c7a478ec868c659933a04cefb4769b7
Richard Ellicott <mescalin@gmail.com>
2022-11-18 15:48:32 +0000

    Update search_for_nodes.gd

--- code_patterns/search_for_nodes.gd
+++ code_patterns/search_for_nodes.gd
@@ -11,30 +11,7 @@ get_tree().get_root().find_node("Name819", true)
 
 
 # https://godotengine.org/qa/17524/how-to-find-an-instanced-scene-by-its-name
-func _find_node_by_name(root, name):
-    """
-    finds a node by name in all childs of root
-    intended to be used for global find function:
-        mylib.find(name)
-    """
-    if(root.get_name() == name):
-        return root
-    for child in root.get_children():
-        if(child.get_name() == name):
-            return child
-        var found = _find_node_by_name(child, name)
-        if(found):
-            return found
-    return null
 
-func find(name):
-    """
-    global find by name (searches whole scene)
-    """
-    var node = _find_node_by_name(get_tree().get_root(), name)
-    if not node:
-        print('WARNING: no object found named ""%s"' % name)
-    return node
 
 
 
@@ -56,7 +33,8 @@ func find_node_by_name(root, _name, max_depth = 3, depth = 0):
                 return child
             var found = find_node_by_name(child, _name, max_depth, depth + 1)
             if(found):
-                retu
+                return found
+    return null
     
     
     
