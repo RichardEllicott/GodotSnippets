38a7413c0bc5b8af876c8cb2392d7750c1ec2cee
Richard Ellicott <mescalin@gmail.com>
2022-11-22 04:05:38 +0000

    Update _check_if_mouse_hovers_control.gd

--- code_patterns/_check_if_mouse_hovers_control.gd
+++ code_patterns/_check_if_mouse_hovers_control.gd
@@ -8,20 +8,27 @@ works even when dragging with mouse down over all controls, ineffecient but good
 """
 
 
+## assign Camera2D
+export (NodePath) var _camera2D : NodePath
+var camera2D : Camera2D
+func get_camera_2D() -> Camera2D:
+    if not camera2D:
+        camera2D = get_node(_camera2D)    
+    return camera2D
+    
+    
 static func _check_if_mouse_hovers_control(control : Control, camera2D : Camera2D, viewport : Viewport) -> bool:
     ## works even when dragging, must check all childs with this function
     ## took a while to work these offsets out so saved as a function
+    ## example:
+    ## _check_if_mouse_hovers_control(child,get_camera_2D(),get_viewport()):
     
     var mouse_position : Vector2 = viewport.get_mouse_position()
-    
     var control_global_rect : Rect2 = control.get_global_rect()
-    
     ## compensates if the child is scaled
     control_global_rect.size *= control.rect_scale ## if the drag child is scaled the rect needs scaling
-    
     ## compensates for camera pos
     control_global_rect.position -= camera2D.position ## correct for camera pos
-    
     if camera2D.anchor_mode == 1: ## if we have a center screen we must offset the pos
         mouse_position -= viewport.get_visible_rect().size/2.0
         
