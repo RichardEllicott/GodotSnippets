9ac95c628d42dc99ddda7589f59626f73704fd89
Richard Ellicott <mescalin@gmail.com>
2023-01-31 19:59:40 +0000

    Update mouse_plane_pos.gd

--- physics/mouse_plane_pos.gd
+++ physics/mouse_plane_pos.gd
@@ -6,6 +6,10 @@ not really a physics function but gets the plane position of the mouse in a 3D w
 
 
 func get_mouse_plane_pos() -> Vector3:
+
+       # INACCURATE!
+       # prob will delete but just found new one below
+
     """
     get the mouse position on a plane
     
@@ -24,3 +28,24 @@ func get_mouse_plane_pos() -> Vector3:
     var result = plane.intersects_ray(from,to)
     
     return result
+
+
+
+static func get_mouse_plane_pos(var camera: Camera, mouse_pos: Vector2):
+    """
+    get the mouse position on a plane (Vector3)
+    
+    WARNING, watch out for null being returned
+    
+    this plane faces upwards (0,1,0) and has an origin at (0,0,0)
+    
+    useful for finding where on a flat floor the mouse is pointing with no raycast
+
+    
+    """
+
+    # https://godotengine.org/qa/25922/how-to-get-3d-position-of-the-mouse-cursor
+    var plane  = Plane(Vector3(0, 1, 0), 0)
+    var from = camera.project_ray_origin(mouse_pos)
+    var to = camera.project_ray_normal(mouse_pos)
+    return plane.intersects_ray(from, to)
